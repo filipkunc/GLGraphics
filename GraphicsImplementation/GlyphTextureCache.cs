a714dd6e3f178118799dca6e3d40cfe977b8e7e7
unknown <Filip@.(none)>
2011-05-21 13:54:44 +0200

    Forgot to add GlyphTextureCache.cs and Helpers.cs

---
+++ GraphicsImplementation/GlyphTextureCache.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using GLWrapper;
+using System.Windows.Forms;
+
+namespace GraphicsImplementation
+{
+    public class GlyphTextureCache : IDisposable
+    {
+        class GlyphInfo
+        {
+            public Rectangle GlyphRect;
+            public SizeF RealSize;
+        };
+
+        Dictionary<char, GlyphInfo> _glyphCoords;
+        GLTexture _texture;
+        Bitmap _bitmap;
+        Point _lastTextureCoord;
+        int _maxY;
+        int _lastGlyphCount;
+        Color _backColor;
+
+        public GlyphTextureCache(Color backColor)
+        {
+            _backColor = backColor;
+            _glyphCoords = new Dictionary<char, GlyphInfo>();
+            _texture = new GLTexture();
+            _bitmap = new Bitmap(512, 512);
+            using (Graphics g = Graphics.FromImage(_bitmap))
+            {
+                g.Clear(_backColor);
+            }
+            _lastTextureCoord = new Point(1, 1);
+            _lastGlyphCount = 0;
+            _maxY = 1;
+        }
+
+        private void AddString(string text, Font font, Brush brush)
+        {
+            using (Graphics g = Graphics.FromImage(_bitmap))
+            {
+                StringFormat sf = StringFormat.GenericTypographic;
+                sf.FormatFlags = StringFormatFlags.MeasureTrailingSpaces;
+
+                foreach (var c in text)
+                {
+                    if (_glyphCoords.ContainsKey(c))
+                        continue;
+
+                    string s = c.ToString();
+                    var size = g.MeasureString(s, font, PointF.Empty, sf);
+                    Size integerSize = new Size((int)size.Width + 1, (int)size.Height + 1);
+                    g.DrawString(s, font, brush, _lastTextureCoord, sf);
+                    var rc = new Rectangle(_lastTextureCoord, integerSize);
+
+                    _glyphCoords.Add(c, new GlyphInfo { GlyphRect = rc, RealSize = size });
+
+                    _lastTextureCoord.X += rc.Width;
+                    _maxY = Math.Max(_maxY, rc.Bottom);
+
+                    if (_lastTextureCoord.X > _bitmap.Width)
+                    {
+                        _lastTextureCoord.X = 1;
+                        _lastTextureCoord.Y = _maxY + 1;
+                    }
+                }
+            }
+        }
+
+        private void UpdateTextureIfNeeded()
+        {
+            if (_lastGlyphCount != _glyphCoords.Count)
+            {
+                using (Bitmap copy = new Bitmap(_bitmap))
+                {
+                    copy.MakeTransparent(_backColor);
+                    _texture.Update(copy, _bitmap.Width, _bitmap.Height);
+                }
+                _lastGlyphCount = _glyphCoords.Count;
+            }
+        }
+
+        public void DrawBitmap(Graphics g)
+        {
+            g.DrawImage(_bitmap, Point.Empty);
+        }
+
+        public void DrawTexture(GLCanvas g)
+        {
+            _texture.Draw(Point.Empty);
+        }
+
+        public void DrawString(IGraphics g, string text, Font font, Brush brush, PointF location)
+        {
+            int oldCount = _glyphCoords.Count;
+            AddString(text, font, brush);
+            if (g is GLGraphics)
+                UpdateTextureIfNeeded();
+
+            List<RectangleF> glyphDst = new List<RectangleF>();
+            List<Rectangle> glyphSrc = new List<Rectangle>();
+
+            location.X += 2.0f; // magic ... really don't know how to get this offset from MeasureString
+
+            foreach (var c in text)
+            {
+                var glyphCoord = _glyphCoords[c];
+
+                var glyphSize = glyphCoord.RealSize;
+                glyphSize.Width = (float)Math.Ceiling(glyphSize.Width);
+                glyphSize.Height = (float)Math.Ceiling(glyphSize.Height);
+
+                glyphDst.Add(new RectangleF(location, glyphSize));
+                glyphSrc.Add(glyphCoord.GlyphRect);
+
+                if (g is GDIGraphics)
+                    g.DrawImage(_bitmap, location.X, location.Y, glyphCoord.GlyphRect.ToRectangleF(), GraphicsUnit.Pixel);
+
+                location.X += (float)Math.Round(glyphCoord.RealSize.Width + 0.2f); // another magic
+            }
+
+            if (g is GLGraphics)
+                _texture.DrawGlyphs(glyphDst, glyphSrc);
+        }
+
+        public void Dispose()
+        {
+            _texture.Dispose();
+        }
+    }
+}
---
+++ GraphicsImplementation/Helpers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using GLWrapper;
+
+namespace GraphicsImplementation
+{
+    public static class Helpers
+    {
+        public static int NextPow2(int n)
+        {
+            int x = 2;
+            while (x < n)
+                x <<= 1;
+            return x;
+        }
+
+        public static bool FloatEquals(float a, float b)
+        {
+            if (Math.Abs(a - b) < 0.01f)
+                return true;
+            return false;
+        }
+
+        public static GLTexture GdiToTexture(Color backColor, Size originalSize, Action<Graphics> draw)
+        {
+            Size power2Size = new Size(Helpers.NextPow2(originalSize.Width), Helpers.NextPow2(originalSize.Height));
+            using (Bitmap bitmap = new Bitmap(power2Size.Width, power2Size.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    if (backColor != Color.Transparent)
+                        g.Clear(backColor);
+                    draw(g);
+                }
+                if (backColor != Color.Transparent)
+                    bitmap.MakeTransparent(backColor);
+                GLTexture texture = new GLTexture(bitmap, originalSize.Width, originalSize.Height);
+                return texture;
+            }
+        }
+
+    }
+}
